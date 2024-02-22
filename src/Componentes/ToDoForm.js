c5f782854bda4fd44eeb53a1296bf532206230bc
Gloria Nayeli Jaime Monreal <pjaimemonreal@gmail.com>
2024-02-22 11:45:10 -0600

    Se agrego la funcionalidad de agregar tareas a la lista

--- src/Componentes/ToDoForm.js
+++ src/Componentes/ToDoForm.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function ToDoForm() {
+export default function ToDoForm({ addTodo }) {
+  const [values, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    addTodo(values);
+    setValue("");
+  };
   return (
     <div>
-      <form className="TodoForm">
-        <input type="text" className="ToDoInput" placeholder="Task"></input>
+      <form className="TodoForm" onSubmit={handleSubmit}>
+        <h1 className="ListTitle">ToDoList</h1>
+        <input
+          type="text"
+          className="ToDoInput"
+          placeholder="Task"
+          onChange={(e) => setValue(e.target.value)}
+          value={values}
+        ></input>
         <button type="submit" className="SubmitButton">
           Agregar
         </button>
--- src/Componentes/ToDoWrapper.js
+++ src/Componentes/ToDoWrapper.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import ToDoForm from "./ToDoForm";
-
+import { v4 as uuidv4 } from "uuid";
+import ToDo from "./ToDo";
+uuidv4();
 export default function ToDoWrapper() {
+  const [todos, SetTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    SetTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ]);
+  };
   return (
     <div>
-      <ToDoForm></ToDoForm>
+      <ToDoForm addTodo={addTodo}></ToDoForm>
+      {todos.map((todo, i) => {
+        return <ToDo task={todo} key={i}></ToDo>;
+      })}
     </div>
   );
 }
